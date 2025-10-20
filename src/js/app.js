fa5a480f02949e8d4fe6d78555d12aa6a8f3c5b3
elalince <118118942+elalince@users.noreply.github.com>
2025-10-20 20:34:46 +0300

    insets

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.showNav();
+flsFunctions.flourish();
 flsFunctions.universities();
 flsFunctions.factsPopup();
 flsFunctions.mainSlider();
 flsFunctions.insetSlider();
 flsFunctions.test();
-flsFunctions.flourish();
 flsFunctions.footer();
 flsFunctions.bodyClacc();
 
